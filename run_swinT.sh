f4e84909cfb68320c0de57385bfc164bbc9026c4
Dr. Pan Huang <59990648+Baron-Huang@users.noreply.github.com>
2025-11-27 01:10:05 +0800

    Delete run_swinT.sh

--- run_swinT.sh
+++
@@ -1,8 +0,0 @@
-
-#
-for seed in 3407
-do
-    echo "Running with random_seed=$seed"
-    python HCFMIL_main.py --abla_type sota --run_mode test --random_seed ${seed} --feat_extract\
-            --bags_len 1025
-done
